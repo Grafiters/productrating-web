a4d35ef3d77555ba5b9f38098037c551fb288bfa
Grafiters <bayugrafit@gmail.com>
2020-12-30 02:53:52 +0700

    feat(controller): edited update defstroy new into purchases

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,16 @@
 class PurchasesController < ApplicationController
+
   before_action do
     @product = Product.find(params[:product_id])
 
     case action_name.to_sym
     when :new, :create
       @purchase = @product.purchases.new
-    when :show
+    when :show, :edit, :update
       @purchase = @product.purchases.find(params[:id])
+      @review = @purchase.review.find(params[:id])
+    when :destroy
+      @purchase = Purchase.find(params[:id])
     end
   end
 
@@ -32,10 +36,18 @@ class PurchasesController < ApplicationController
 
   def update
     # TODO: Update record (save to database)
+    if @purchase.update_attributes(purchase_params)
+      redirect_to product_url(@product)
+    else
+      flash[:error] = @purchase.errors.full_messages.join(', ')
+      render :new
+    end
   end
 
   def destroy
     # TODO: Delete record
+    @purchase.destroy
+    redirect_to product_ur(@product)
   end
 
   def show
@@ -45,4 +57,6 @@ class PurchasesController < ApplicationController
     def purchase_params
       params.require(:purchase).permit(:quantity, :delivery_address)
     end
+
+    
 end  
