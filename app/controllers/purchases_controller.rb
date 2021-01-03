f1e364d20c9f0373fd17dfacf64a59b7113880cc
Grafiters <bayugrafit@gmail.com>
2021-01-03 14:25:54 +0700

    feat(controller): edited view

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,9 +6,9 @@ class PurchasesController < ApplicationController
     case action_name.to_sym
     when :new, :create
       @purchase = @product.purchases.new
-    when :show, :edit, :update
+    when :show, :edit, :update, :create
       @purchase = @product.purchases.find(params[:id])
-      @review = @purchase.review.find(params[:id])
+      @review = @purchase.review.find_by(params[:purchase_id])
     when :destroy
       @purchase = Purchase.find(params[:id])
     end
@@ -21,9 +21,14 @@ class PurchasesController < ApplicationController
     # TODO: Also decrease product quantity.
     # - For example, if `purchase.quantity` is 3, decrease `product.quantity` by 3
     # - Display an error if `product.quantity` is less than 0 (negative number)
+    # clear
     @purchase.assign_attributes(purchase_params)
+    @quantity = @purchase.quantity
+    @product.quantity -= @quantity
     if @purchase.save
-      redirect_to product_url(@product)
+      if @product.update_attributes(product_params)
+        redirect_to product_url(@product) 
+      end
     else
       flash[:error] = @purchase.errors.full_messages.join(', ')
       render :new
@@ -32,10 +37,12 @@ class PurchasesController < ApplicationController
 
   def edit
     # TODO: Show edit form
+    # clear
   end
 
   def update
     # TODO: Update record (save to database)
+    # cleat
     if @purchase.update_attributes(purchase_params)
       redirect_to product_url(@product)
     else
@@ -46,11 +53,15 @@ class PurchasesController < ApplicationController
 
   def destroy
     # TODO: Delete record
+    #clear
     @purchase.destroy
     redirect_to product_ur(@product)
   end
 
   def show
+    # render json: {
+      
+    # }
   end
 
   private
@@ -58,5 +69,7 @@ class PurchasesController < ApplicationController
       params.require(:purchase).permit(:quantity, :delivery_address)
     end
 
-    
+    def product_params
+      params.permit(:quantity)
+    end
 end  
